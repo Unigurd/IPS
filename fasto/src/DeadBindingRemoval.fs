55e77f9f9907f79c31927a76b621f3654bc6cfe6
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2019-05-24 12:54:29 +0200

    tilfoejet kode til rapporten for task 3

--- fasto/src/DeadBindingRemoval.fs
+++ fasto/src/DeadBindingRemoval.fs
@@ -61,39 +61,14 @@ let rec removeDeadBindingsInExp (e : TypedExp) : (bool * DBRtab * TypedExp) =
 
         (* TODO: Task 3: implement the cases of `Var`, `Index` and `Let` expressions below *)
         | Var (name, pos) ->
-            (* Task 3, Hints for the `Var` case:
-                  - 1st element of result tuple: can a variable name contain IO?
-                  - 2nd element of result tuple: you have discovered a name, hence
-                        you need to record it in a new symbol table.
-                  - 3rd element of the tuple: should be the optimised expression.
-            *)
-            (false, SymTab.fromList [name,()], Var (name, pos))
+           (false, SymTab.fromList [name,()], Var (name, pos))
 
         | Index (name, e, t, pos) ->
-            (* Task 3, `Index` case: is similar to the `Var` case, except that,
-                        additionally, you also need to recursively optimize the index 
-                        expression `e` and to propagate its results (in addition
-                        to recording the use of `name`).
-            *)
-            let (ios, uses, e') = removeDeadBindingsInExp e
+           let (ios, uses, e') = removeDeadBindingsInExp e
             (ios, SymTab.bind name () uses, Index (name, e', t, pos)) 
 
         | Let (Dec (name, e, decpos), body, pos) ->
-            (* Task 3, Hints for the `Let` case:
-                  - recursively process the `body` of the let-binding.
-                  - if `name` was not found to have been used in `body` 
-                    then simply return the result obtained from the body
-                    otherwise
-                  - recursively process the binded expression `e` and
-                    unifying the two results (for `body` and for `e`).
-                      -- unifying whether the two sub-exps exhibit
-                         IO operations: either one OR the other ...
-                      -- unifying the used-names: semantically union
-                         the two symbol tables (`SymTab.combine`)
-                      -- construct the the new `Let` expression from
-                         the resulted optimized subexpressions.
-            *)
-            let (bodyios, bodyuses, body') = removeDeadBindingsInExp body
+           let (bodyios, bodyuses, body') = removeDeadBindingsInExp body
             match SymTab.lookup name bodyuses with
                 | Some _ ->
                     let eios, euses, e' = removeDeadBindingsInExp e
